065c1fd4d3b300290f9a7c2a9bf9db864a1e3ab2
splitcentre <raihanalfianpratama@gmail.com>
2023-10-20 07:34:56 +0700

    change view and controller

--- app/Http/Controllers/Auth/LoginRegisterController.php
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -2,63 +2,82 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Session\Store;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\ValidationException;
+
 
 class LoginRegisterController extends Controller
 {
-    public function __construct(){
-        $this->middleware("guest")->except{[
-            'logout','dashboard'
-        ]};
+    public  function __construct()
+    {
+        $this->middleware('guest')->except(['logout', 'dashboard']);
     }
-    public function register(Request $request){
+
+    public function register()
+    {
         return view('auth.register');
     }
-    public function store(request $request){
+
+    public function Store (Request $request){
         $request->validate([
-            'name'=> 'required|string|max:250',
-            'email'=> 'required|email|max:250|unique:users',
-            'password'=> 'required|min:8|confirmed'
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
         ]);
-        $user = User::create([
-            'name'=> $request->name,
-            'email'=> $request->email,
-            'password'->Hash::make($request->password)
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=> hash::make($request->password)
         ]);
-        $credential=$request->only('email','password');
-        Auth::attempt($credential);
+
+        $credentials = $request->only('email','password');
+        Auth::attempt($credentials);
         $request->session()->regenerate();
-        return redirect()->route('dashboard')->withSuccess('Succesfully Registered & Logged');
+        return redirect()->route('dashboard')
+        ->withSuccess("Welcome! You have Successfully loggedin");
     }
-    public function login(){
-        return view('auth.login');
-    }
-    public function authenticate(Request $request){
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
 
-        if (Auth::attempt($credentials)) {
-            // Authentication was successful
-            $request->session()->regenerate();
-            return redirect('/home')->with('success', 'Login Success');
+   public function login(){
+         return view('auth.login');
+   } 
+
+   public function authenticate(Request $request){
+         $credentials = $request->validate([
+                'email' => 'required', 'email',
+                'password' => 'required',
+         ]);
+
+         if(Auth::attempt($credentials)){
+              $request->session()->regenerate();
+              return redirect()->route('dashboard')
+              ->withSuccess("Welcome! You have Successfully loggedin");
+         }
+         return back()->withErrors([
+              'email' => 'The provided credentials do not match our records',
+         ])->onlyInput('email');
+   }
+
+    public function dashboard(){
+        
+        if (Auth::check()) {
+            return view('auth.dashboard');
         }
+        return redirect()->route('login')
+        ->withErrors(['You are not allowed to access',])->onlyInput('email');
 
-        // Authentication failed
-        return back()->withErrors('error', 'Incorrect Email or Password');
     }
+
     public function logout(Request $request){
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login')->withSuccess('You Have Been Logged Out');
+        return redirect()->route('login')
+        ->withSuccess("You have logged out");
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+   });

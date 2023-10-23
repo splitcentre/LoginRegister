a06f790bb510fbfbab4c914935dd76d097d2378b
splitcentre <raihanalfianpratama@gmail.com>
2023-10-23 17:22:58 +0700

    change view

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,20 +1,22 @@
-    @extend('auth.layouts')
+@extends('auth.layouts')
 
-    @section('content')
-
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="class-body">
-                    @if ($message=Session::get('success'))
-                    <div class="alert alert-success">{{$message}}</div>
-                    @else
-                    <div class="alert alert-success">You Logged in!!!</div>
-                    @endif
-                </div>
+@section('content')
+<div class="row justify-content-center mt-5">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Dashboard</div>
+            <div class="card-body">
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        {{ $message }}
+                    </div>
+                @else
+                    <div class="alert alert-success">
+                        You are logged in!
+                    </div>
+                @endif
             </div>
         </div>
     </div>
-
-    @endsection
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginRegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*

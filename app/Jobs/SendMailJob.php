ea7640315af5d5951be95bf90e20c55d7bf07371
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-10-31 09:57:42 +0700

    add mail controller and job

---
+++ app/Jobs/SendMailJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\send_mail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmail;
+
+class SendMailJob
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $email = new send_mail($this->data);
+        Mail::to($this->data['email'])->send($email);
+    }
+}
